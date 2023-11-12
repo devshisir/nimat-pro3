e0f13895592ce19c9163437e748b9b0476d3518d
devshisir <contact.shisirimran@gmail.com>
2023-11-13 01:59:00 +0600

    Bar code popup update

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-    $('select').niceSelect();
+    // $('select').niceSelect();
     $('.toggle_button').on('click',function(){
         $('.appLayout').toggleClass('toggle');
     })
 
-    $('select').on('change',function(){
-        var selectFirst = $('.current').html(); 
-        if (selectFirst.indexOf("Select") >= 0){
-            $(this).addClass('text-gray')
-        }else{
-            $(this).removeClass('text-gray')
-        }
-    })
+    // $('select').on('change',function(){
+    //     var selectFirst = $('.current').html(); 
+    //     if (selectFirst.indexOf("Select") >= 0){
+    //         $(this).addClass('text-gray')
+    //     }else{
+    //         $(this).removeClass('text-gray')
+    //     }
+    // })
 
     if ($(".current")[0]){
         var selectFirst = $('.current').html(); 
