e3534eafaa5f93e7c1a9fc9e4f0b2a0bd803be39
devshisir <contact.shisirimran@gmail.com>
2023-11-27 12:53:53 +0600

    Listing page add

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,29 +1,28 @@
-$(document).ready(function() {
-    // $('select').niceSelect();
-    $('.toggle_button').on('click',function(){
-        $('.appLayout').toggleClass('toggle');
-    })
+$(document).ready(function () {
+  // $('select').niceSelect();
+  $(".toggle_button").on("click", function () {
+    $(".appLayout").toggleClass("toggle");
+  });
 
-    // $('select').on('change',function(){
-    //     var selectFirst = $('.current').html(); 
-    //     if (selectFirst.indexOf("Select") >= 0){
-    //         $(this).addClass('text-gray')
-    //     }else{
-    //         $(this).removeClass('text-gray')
-    //     }
-    // })
+  // $('select').on('change',function(){
+  //     var selectFirst = $('.current').html();
+  //     if (selectFirst.indexOf("Select") >= 0){
+  //         $(this).addClass('text-gray')
+  //     }else{
+  //         $(this).removeClass('text-gray')
+  //     }
+  // })
 
-    if ($(".current")[0]){
-        var selectFirst = $('.current').html(); 
-        if (selectFirst.indexOf("Select") >= 0){
-            $(this).addClass('text-gray')
-        }else{
-            $(this).removeClass('text-gray')
-        }
+  if ($(".current")[0]) {
+    var selectFirst = $(".current").html();
+    if (selectFirst.indexOf("Select") >= 0) {
+      $(this).addClass("text-gray");
+    } else {
+      $(this).removeClass("text-gray");
     }
+  }
 
-
-    
-
-    
+  $("select").each(function () {
+    $(this).addClass("form-select");
+  });
 });
